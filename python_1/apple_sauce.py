d56c97e3c32e11990f4987880b0cd44ed37f3082
Thai-Stars <thai.dupe47@gmail.com>
2024-11-06 16:51:24 -0500

    today I learned how to create varibles

---
+++ python_1/apple_sauce.py
@@ -0,0 +1,5 @@
+print("Welcome to APPLE SACUCE program")
+
+numberOfUserAppleSauce = input ("How many apple sacues do you want:")
+
+print("user said," , numberOfUserAppleSauce)
---
+++ python_1/thai.py
@@ -0,0 +1,23 @@
+#camelcase
+firstName = "Thai" #This is a string
+
+#snakecase
+
+last_name = "Dupervil" #This is a string
+nickName = "Thailand" #This is a string
+
+age = 17 # this is an interger 
+bankAccountBalance = 500000.00 #this is a float
+
+print(firstName)
+print(bankAccountBalance)
+
+if bankAccountBalance > 100:
+    print ("YOURE STACKED ZOO WEE MAMA")
+elif bankAccountBalance < 100: 
+    print("ur a brokie")
+    
+
+
+
+
